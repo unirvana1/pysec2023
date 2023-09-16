6748abcf0beda929525a3eba9f3210b9c595dc3e
unirvana1 <ugis.m@tvnet.lv>
2023-09-16 11:11:31 +0300

    Rezirēķina spikeris

---
+++ Conditional statements/For.py
@@ -0,0 +1,8 @@
+#reizrēķina špikeris
+number = int("ievadi skaitli:")
+
+
+for reizināšana in range(1,11):
+    rezultāts = number * reizināšana
+    print(number, "x", reizināšana, "=", rezultāts)
+    
